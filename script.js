c0163d2784fdde668f38424d9005bed7f07bf172
Antony Pinto Avila <113103345+Ju1ioluliob@users.noreply.github.com>
2022-11-27 17:30:34 +0100

    fixed create user form

--- script.js
+++ script.js
@@ -7,121 +7,79 @@ let register = document.getElementById("newUser")
 let logoutBtn = document.createElement("button")
 let removeLogout = document.getElementById("userLogout")
 let head = document.getElementById("head")
+let signForm = document.getElementById("createForm")
+let aa = document.getElementById("aa")
+let createUser = document.getElementById("createUser")
+
+styleLogBtn()
 
 
 
+signForm.style.display = "none"
+aa.style.display = "none"
 
 
-//  if(localStorage.getItem("usersLogin")) {
-//     console.log("är inloggad finns ls")
-//     let noUser =[
-//         {
-//             username: "", 
-//         password: ""
-//         },   
-//     ];
-
-//     localStorage.setItem("userLogin", JSON.stringify(noUser))
-//     logged()
-//  } else {
-//     console.log("inte inloggad iget ls")
-//     let users = [
-//         {
-//             username: "janne", 
-//         password: "test"
-//         },
-//         {
-//             username: "tony", 
-//         password: "montana"
-//          }
-//     ];
-    
-//    localStorage.setItem("usersLogin", JSON.stringify(users))
-// logged()
-//  }
-
-//  function logged () {
-//     document.body.classList = localStorage.getItem("usersLogin")
-//     showButoon()
-//  }
- 
-//  function showButoon () {
-//     let toggleBtn = document.createElement("button")
-//     toggleBtn.innerText = ""
-//  let users =  localStorage.getItem("usersLogin")
-
-// for (i = 0; i < users.length; i++) {
-//     if (username.value == users[i].username && password.value == users[i].password /*|| username === "tony" && pass === "montana" */) {
-//         //                 console.log("You have succesfully logged in " + username.value)
-                        
-//         //                 console.log(users)        // localStorage.setItem("usersLogin", "noUser")
-//         // logged()
-//         toggleBtn.innerText = "logout"
-
-//     } else {
-//         toggleBtn.innerText = "login"
-
-//         // localStorage.setItem("usersLogin", "users")
-//         // logged()
-//     }
-//     head.appendChild(toggleBtn)
-// }
-// }
-
-
-// // skapa inloggare
+window.localStorage = localStorage.getItem("logUser".logUser)
+// styleLogBtn()
+// let logUser = []
+// tillgängliga inloggar
 let users = [
     {
-        username: "janne", 
+     id:1,   username: "janne", 
     password: "test"
     },
     {
-        username: "tony", 
+     id:2,   username: "tony", 
     password: "montana"
      }
 ];
-// localStorage.setItem("users",JSON.stringify(users))
-// console.log(users)
-//  log1()
-   
+// localStorage.users = JSON.stringify(users)
 
 
-if (localStorage.getItem("logUser")) {
+if (localStorage.getItem("online")) {
     console.log("Det finns LS")
-    // let users = JSON.parse(localStorage.getItem("users"))
-    // localStorage.setItem("users", users)
-    
-noLogin()
-    // log1()
 
-    
+
+noLogin()
+
 
 }else {
     console.log("finns inget LS logccccccccccccga in")
- // sätt localstorage
+
+    localStorage.setItem("logUser", JSON.stringify(users))
+
+// localStorage.setItem("allUsers",JSON.stringify(logUser))
  
-//  localStorage.setItem("users", JSON.stringify(users))
- log1()
+log1()
+
 
 }
-// let usersReg = JSON.parse(localStorage.getItem("users"))
+// users = JSON.parse(localStorage.getItem("logUser"))
 
 function log() {
-    let users = JSON.parse(localStorage.getItem("users"))
+    // localStorage.setItem("logUser", JSON.stringify(users))
+    let users = JSON.parse(localStorage.getItem("logUser"))
+    
 for (i = 0; i < users.length; i++) {
-    if (inputUsername.value == users[i].username && inputPassword.value == users[i].password) {
+    if (inputUsername.value === users[i].username && inputPassword.value === users[i].password || localStorage.getItem("logUser") === inputUsername.value && inputPassword.value) {
                 console.log("You have succesfully logged in " + inputUsername.value)
                 head.style.display = "none"
                 console.log(users)
-                let logUser = inputUsername.value
+                let logUser = localStorage.getItem("logUser")
                 localStorage.setItem("logUser", logUser)
+                let online = inputUsername.value
+                localStorage.setItem("online",online)
+                aa.style.display = "block"
+
+                // logUser = localStorage.users
+                // localStorage.setItem("newUser",JSON.stringify(users))
                 noLogin()
-    // console.log(password.value)
     return
-    } else { 
+    } 
+    else{ 
                 
-                // showText=document.createElement("h2")
                 showText.innerText= "The user does not exist"
+                showText.style.color= "red"
      console.log("not a valid account")
             head.appendChild(showText)
             
@@ -137,29 +95,30 @@ function setlocal() {
 // logga in användare 
 function log1() {
 login.addEventListener("click", ()=> {
-    // evt.preventDefault()
     console.log("Click on button")
     log()
 })
 }
 
-// stnna inloggad
 
 
-
-
-
-
-
-// tar bort section i header alltså login funktion
+// hemsida visas inloggad
 
 function noLogin(){
+    // localStorage.setItem("users", JSON.stringify(users))
 
     head.style.display = "none"
     let logoutBtn = document.createElement("button")
+    let welcome = document.createElement("h2")
+    welcome.innerText= "HI welcome to this page"
+
+    welcome.style.position = "relative"
+    welcome.style.top = "26px"
+    welcome.style.left = "51px"
+    
 
 
-//  skapa och skriv ut en logga ut knapp
+// newUserkapa och skriv ut en logga ut knapp
 logoutBtn.innerText = "Logout"
 
 // flttar kanppen till höger
@@ -170,87 +129,142 @@ logoutBtn.style.position= "fixed"
 
 // // visa den som är inloggad
 let loggedUser = document.createElement("h2")
-loggedUser.innerText= "Inlogged as: " + localStorage.getItem("logUser");
+loggedUser.innerText= "Inlogged as: " + localStorage.getItem("online");
 
 // brevid logoutknapp
 
 loggedUser.style.position= "fixed"
 loggedUser.style.left= "78%"
-loggedUser.style.top= "9px"
+loggedUser.style.top= "22px"
 loggedUser.style.color= "grey"
 
 
-document.body.appendChild(logoutBtn)
+aa.appendChild(logoutBtn)
 
-document.body.appendChild(loggedUser)
+aa.appendChild(loggedUser)
+aa.appendChild(welcome)
 
     // lägger till funkiton logga ut 
     logoutBtn.addEventListener("click", () => {
         showText.innerText= ""
         console.log("knapp igenom")
         head.style.display = "block"
-        localStorage.removeItem("logUser")
+        localStorage.setItem("users", JSON.stringify(users))
+        localStorage.removeItem("online")
         
-        location.reload()
+        // location.reload()
+        aa.style.display = "none"
+
         logoutBtn.style.display= "none"
         loggedUser.style.display= "none"
+        welcome.style.display= "none"
         console.log("goodbye")
  })
-
-    }
-window.onload = function () {
-    localStorage.getItem("users")
 }
         
 //  // skapa ny användare
 
 
-//  register.addEventListener ("click", () => {
+ register.addEventListener ("click", () => {
+    signForm.style.display = "block"
+    aa.style.display = "none"
+    head.style.display = "none"
 
-//     // hämta
-//     let users = JSON.parse(localStorage.getItem("users"))
-// // gör ny användare
-//     let newUsername = username.value
-//     let newPassword = password.value
-//     let newUser = { 
-//         username: newUsername,
-//         password: newPassword
-//     }
 
-//     for (i=0 ; i < users.length; i++) {
-//         if(newUsername == users[i].username) {
-//             console.log("taken")
-//             return
+    // hämta
+    let logUser = JSON.parse(localStorage.getItem("logUser"))
+// gör ny användare
+    let newUsername = inputUsername.value
+    let newPassword = inputPassword.value
+    let newUser = { 
+        id: users.length +1,
+        username: newUsername,
+        password: newPassword
+    }
 
-//         } 
-        
-//     }
-//     console.log("newUser", newUser)
+    for (i=0 ; i < users.length; i++) {
+        if(newUsername === users[i].username) {
+            console.log("taken")
+            return
 
-//     // ändra
-//     users.push(newUser)
-//     //spara
-//     localStorage.setItem("users", JSON.stringify(users))
+        } else{ 
+    console.log("newUser", newUser)
 
-//    console.log("User", users)
-//     }
-//  )
+    // ändra
+   
+    logUser.push(newUser)
 
-// //  function clean() {
+    //spara
+    localStorage.setItem("logUser", JSON.stringify(logUser))
+    // localStorage.setItem("",JSON.stringify(newUser))
+
+   console.log("User", users)
+        }
+    }
+} )
+
+
+
+
+
+
+function styleLogBtn (){
+
+    // head.style.background = " #1c1c1c"
+    // head.style.borderRadius = " 8px"
+    // head.style.overflow = "hidden"
+    // head:before.style.background = " #1c1c1c"
+    // head.style.background = " #1c1c1c"
+    // head.style.background = " #1c1c1c"
+    // head.style.background = " #1c1c1c"
+    // head.style.background = " #1c1c1c"
+    // head.style.background = " #1c1c1c"
+    // head.style.background = " #1c1c1c"
+
+
+
+
+    login.style.position = "relative";
+    login.style.display = "inline-block";
+    login.style.padding = "12px 36px";
+    login.style.color = "#fff";
+    login.style.textDecoration = "none";
+    login.style.textTransform = "uppercase";
+    login.style.fontSize = "18px";
+    login.style.letterSpacing = "2px";
+    login.style.borderRadius = "40px";
+    login.style.background = "linear-gradient(90deg, #0162c8, #55e7fc)";
+    login.style.border = "none";
+    login.style.cursor = "pointer";
+    login.style.boxShadow = "0px 5px darkblue";
+    login.style.boxShadowColor = "black";
     
+    newUser.style.position = "relative";
+    newUser.style.display = "inline-block";
+    newUser.style.padding = "12px 36px";
+    newUser.style.color = "#fff";
+    newUser.style.textDecoration = "none";
+    newUser.style.textTransform = "uppercase";
+    newUser.style.fontSize = "18px";
+    newUser.style.letterSpacing = "2px";
+    newUser.style.borderRadius = "40px";
+    newUser.style.background = "linear-gradient(90deg, #0162c8, #55e7fc)";
+    newUser.style.border = "none";
+    newUser.style.cursor = "pointer";
+    newUser.style.boxShadow = "0px 5px darkblue"
+    newUser.style.boxShadowColor = "black";
     
-// // inputUs.innerText = ""
-// //     password.innerText = ""
-// //  }
-
-
-// // //  function realod (logintoken) {
-// // //     if (logintoken === null) {
-// // //         logout()
-// // //     } else if (logintoken === "users") {
-// // //         log()
-// // //     }
-// // //  }
-
-// // //  window.onload = realodLog
-// // // visa ny diplay på skärmen
+    createUser.style.padding = "12px 36px";
+    createUser.style.color = "#fff";
+    createUser.style.textDecoration = "none";
+    createUser.style.textTransform = "uppercase";
+    createUser.style.fontSize = "18px";
+    createUser.style.letterSpacing = "2px";
+    createUser.style.borderRadius = "40px";
+    createUser.style.background = "linear-gradient(90deg, #0162c8, #55e7fc)";
+    createUser.style.border = "none";
+    createUser.style.cursor = "pointer";
+    createUser.style.boxShadow = "0px 5px darkblue"
+    createUser.style.boxShadowColor = "black";
+    
+}
